0030e27bb146323b913c5acdae2d541e61bda340
ansadkl10 <anuansad50@gmail.com>
2026-04-20 18:28:31 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: false,
+  // Turbopack എറർ ഒഴിവാക്കാൻ ഇത് സഹായിക്കും
+  turbopack: {}, 
   webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.resolve.fallback = { 
+      fs: false, 
+      net: false, 
+      tls: false,
+      child_process: false 
+    };
     return config;
   },
 }
+
+module.exports = nextConfig;
